5e6a894b563dec07f3801228ededb607330f83ba
Ian L <ileach81@gmail.com>
2022-04-12 11:32:33 -0400

    added table creation for schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,11 @@
 DROP DATABASE IF EXISTS TrackYourTeam_db;
 CREATE DATABASE TrackYourTeam_db;
+-- Uses created db --
+USE TrackYourTeam_db;
+
+-- Create schema tables --
+CREATE TABLE depatment();
+
+CREATE TABLE role();
+
+CREATE TABLE employee();
