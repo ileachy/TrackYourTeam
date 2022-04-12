aeff8d2eb4970f22bde95026cfd5ab755a56565b
Ian L <ileach81@gmail.com>
2022-04-12 11:34:39 -0400

    added department defs

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,10 @@ CREATE DATABASE TrackYourTeam_db;
 USE TrackYourTeam_db;
 
 -- Create schema tables --
-CREATE TABLE depatment();
+CREATE TABLE depatment(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE role();
 
