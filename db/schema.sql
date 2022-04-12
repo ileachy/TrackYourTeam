36db515eeec5ef4e5c60d8154182373671418df4
Ian L <ileach81@gmail.com>
2022-04-12 11:46:58 -0400

    employee values

--- db/schema.sql
+++ db/schema.sql
@@ -9,6 +9,17 @@ CREATE TABLE depatment(
   name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE role();
+CREATE TABLE role(
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER
+);
 
-CREATE TABLE employee();
+CREATE TABLE employee(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER
+);
