9be393cd8de338ee8a3227179ee051c5f5146a98
Ian L <ileach81@gmail.com>
2022-04-15 09:07:39 -0400

    added mysql create connection

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// Global vars
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const conTable = require("console.table");
 const figlet = require("figlet");
 require("dotenv").config();
+
+// MySQL connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "TrackYourTeam_db",
+});
