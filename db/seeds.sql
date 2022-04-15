a339f8300150127389da69310bb9f81d04f452e7
Ian L <ileach81@gmail.com>
2022-04-15 06:36:26 -0400

    added roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,19 @@
 INSERT INTO department (name)
 VALUES
-  ("Warehouse")
-  ("Sales")
-  ("HR")
-  ("Accounting")
-  ("IT")
+  ("Warehouse"),
+  ("Sales"),
+  ("HR"),
+  ("Accounting"),
+  ("IT");
 
 INSERT INTO role (title, salary,department_id)
-VALUES
+VALUES 
+  ("Stocker", 40000, 1),
+  ("Lead Sales", 80000, 2),
+  ("Junior Sales", 50000, 2),
+  ("Recruiter", 75000, 3),
+  ("Accountant", 70000, 4),
+  ("IT Manager", 80000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
